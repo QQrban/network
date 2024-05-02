a511b470a75adb283348827df43401950692bfab
Toomas Vooglaid <tvooglai@noreply.01.kood.tech>
2024-05-02 17:27:22 +0000

    Delete 'frontend/webSocketPluginVite.js'

--- frontend/webSocketPluginVite.js
+++
@@ -1,9 +0,0 @@
-// webSocketPluginVite.js
-import injectSocketIO from './socketIoHandler.js';
-
-export const webSocketServer = {
-    name: 'webSocketServer',
-    configureServer(server) {
-        injectSocketIO(server.httpServer);
-    }
-};

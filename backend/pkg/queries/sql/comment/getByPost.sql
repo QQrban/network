a3ca91d2ec6ec9a4a8b8b31a7ba3789e0f8c474b
toomasv <toomas.vooglaid@gmail.com>
2024-05-20 17:47:32 +0300

    comment ASC

--- backend/pkg/queries/sql/comment/getByPost.sql
+++ backend/pkg/queries/sql/comment/getByPost.sql
@@ -2,5 +2,5 @@ SELECT c.ID, c.aboutID, c.authorID, c.content, c.images, c.created, u.*
 FROM post c
     JOIN user u on u.ID = c.authorID
 WHERE aboutID = ?
-ORDER BY c.ID DESC
+ORDER BY c.ID ASC
 ;

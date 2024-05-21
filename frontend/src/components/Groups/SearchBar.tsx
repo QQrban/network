c61c8b2ac41561a4e84c0cb22c30f84304f09403
Andreea Daniela Burnaz <danielaburnaz@gmail.com>
2024-05-21 14:25:48 +0300

    Added SearchBar for Groups

--- frontend/src/components/Groups/SearchBar.tsx
+++ frontend/src/components/Groups/SearchBar.tsx
@@ -1,8 +1,66 @@
 import { Box, InputBase, IconButton } from "@mui/material";
 import Image from "next/image";
 import searchIcon from "../../../public/icons/search.svg";
+import { fetchFromServer } from "@/lib/api";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+interface AllGroups {
+  id: number;
+  title: string;
+  image: string | null;
+}
 
 export default function SearchBar() {
+  const [searchText, setSearchText] = useState<string>("");
+  const [searching, setSearching] = useState<boolean>(false);
+  const [allGroups, setAllGroups] = useState<AllGroups[]>([]);
+  const [filteredGroups, setFilteredGroups] = useState<AllGroups[]>([]);
+
+  useEffect(() => {
+    const fetchPeople = async () => {
+      const getAllGroups = await fetchFromServer("/groups", {
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const allGroupsData = await getAllGroups.json();
+      const groups: AllGroups[] = allGroupsData.map((group: any) => ({
+        id: group.ID,
+        title: group.title,
+        image: group.image || null,
+      }));
+      setAllGroups(groups);
+      console.log(groups);
+    };
+    fetchPeople();
+  }, []);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (searchText) {
+        setSearching(true);
+        const filtered = allGroups.filter((group) =>
+          `${group.title}`.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setFilteredGroups(filtered);
+      } else {
+        setFilteredGroups([]);
+        setSearching(false);
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchText, allGroups]);
+
+  const clickedLink = () => {
+    setSearchText("");
+    setSearching(false);
+    setFilteredGroups([]);
+  };
   return (
     <Box
       component="form"
@@ -15,11 +73,6 @@ export default function SearchBar() {
         borderRadius: "16px",
       }}
     >
-      <InputBase
-        sx={{ ml: 1, flex: 1, fontFamily: "Schoolbell", fontSize: "21px" }}
-        placeholder="Search for Groups"
-        inputProps={{ "aria-label": "search groups" }}
-      />
       <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
         <Image
           style={{ width: "25px", height: "25px" }}
@@ -27,6 +80,48 @@ export default function SearchBar() {
           alt="search"
         />
       </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1, fontFamily: "Schoolbell", fontSize: "21px" }}
+        placeholder="Search for Groups"
+        inputProps={{ "aria-label": "search groups" }}
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      {searching && (
+        <Box
+          sx={{
+            borderRadius: "0 0 10px 10px",
+            background: "white",
+            position: "absolute",
+            width: 300,
+            height: 150,
+            overflowY: "scroll",
+            border: "2px solid #4b4b4b",
+            borderTop: "none",
+            top: 250,
+            left: 144,
+            padding: "10px",
+            zIndex: 1,
+          }}
+        >
+          {filteredGroups.map((group) => (
+            <Link key={group.id} href={`/groups/${group.id}`}>
+              <Box
+                onClick={clickedLink}
+                sx={{
+                  fontSize: "20px",
+                  "&:hover": {
+                    background: "#dedede",
+                  },
+                  zIndex: 2,
+                }}
+              >
+                {group.title}
+              </Box>
+            </Link>
+          ))}
+        </Box>
+      )}
     </Box>
   );
 }

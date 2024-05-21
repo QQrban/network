b8d2e4e793ff2fe74434fb938af5a3cbe6d00cd6
toomasv <toomas.vooglaid@gmail.com>
2024-05-21 18:30:47 +0300

    unique random

--- backend/pkg/queries/sql/user/suggestedGroups.sql
+++ backend/pkg/queries/sql/user/suggestedGroups.sql
@@ -1,5 +1,10 @@
-SELECT DISTINCT g.* FROM "group" g
-JOIN groupMember gm ON g.ID = gm.groupID
-WHERE gm.userID != ?1
-ORDER BY random()
-LIMIT 3
+SELECT g.*
+FROM (
+    SELECT DISTINCT g.ID 
+    FROM "group" g
+    JOIN groupMember gm ON g.ID = gm.groupID
+    WHERE gm.userID != ?1
+    ORDER BY random()
+    LIMIT 3
+) sub
+JOIN "group" g ON sub.ID = g.ID;

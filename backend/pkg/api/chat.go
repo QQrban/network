822c1de33d3f0f2404b4ca476046ebebcc58c51b
toomasv <toomas.vooglaid@gmail.com>
2024-05-28 18:31:11 +0300

    chatGPT corrections

--- backend/pkg/api/chat.go
+++ backend/pkg/api/chat.go
@@ -15,9 +15,6 @@ import (
 )
 
 var (
-	/**
-	websocketUpgrader is used to upgrade incomming HTTP requests into a persitent websocket connection
-	*/
 	websocketUpgrader = websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
@@ -26,48 +23,33 @@ var (
 	ErrEventNotSupported = errors.New("this event type is not supported")
 )
 
-// checkOrigin will check origin and return true if its allowed
 func checkOrigin(r *http.Request) bool {
-
-	// Grab the request origin
 	origin := r.Header.Get("Origin")
 	fmt.Println("origin", origin)
 
 	switch origin {
-	case "http://localhost:3000": // 3000
+	case "http://localhost:3000":
 		return true
 	default:
 		return false
 	}
 }
 
-// Manager is used to hold references to all Clients Registered, and Broadcasting etc
 type Manager struct {
 	clients ClientList
-
-	// Using a syncMutex here to be able to lcok state before editing clients
-	// Could also use Channels to block
 	sync.RWMutex
-
-	// handlers are functions that are used to handle Events
 	handlers map[string]EventHandler
-	// otps is a map of allowed OTP to accept connections from
-	//otps RetentionMap
 }
 
-// NewManager is used to initalize all the values inside the manager
-func NewManager() *Manager { //ctx context.Context
+func NewManager() *Manager {
 	m := &Manager{
 		clients:  make(ClientList),
 		handlers: make(map[string]EventHandler),
-		// Create a new retentionMap that removes Otps older than 5 seconds
-		//otps: NewRetentionMap(ctx, 5*time.Second),
 	}
 	m.setupEventHandlers()
 	return m
 }
 
-// setupEventHandlers configures and adds all handlers
 func (m *Manager) setupEventHandlers() {
 	m.handlers[EventSendMessage] = func(e Event, c *Client) error {
 		fmt.Println(e)
@@ -75,11 +57,8 @@ func (m *Manager) setupEventHandlers() {
 	}
 }
 
-// routeEvent is used to make sure the correct event goes into the correct handler
 func (m *Manager) routeEvent(event Event, c *Client) error {
-	// Check if Handler is present in Map
 	if handler, ok := m.handlers[event.Type]; ok {
-		// Execute the handler and return any err
 		if err := handler(event, c); err != nil {
 			return err
 		}
@@ -89,252 +68,47 @@ func (m *Manager) routeEvent(event Event, c *Client) error {
 	}
 }
 
-// loginHandler is used to verify an user authentication and return a one time password
-/*func (m *Manager) LoginHandler(w http.ResponseWriter, r *http.Request) {
-	session := getSession(r)
-
-	type userLoginRequest struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-
-	var req userLoginRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Authenticate user / Verify Access token, what ever auth method you use
-	//if req.Username == "percy" && req.Password == "123" {
-	if session != nil {
-		// format to return otp in to the frontend
-		type response struct {
-			OTP string `json:"otp"`
-		}
-
-		// add a new OTP
-		otp := m.otps.NewOTP()
-
-		resp := response{
-			OTP: otp.Key,
-		}
-
-		data, err := json.Marshal(resp)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		// Return a response to the Authenticated user with the OTP
-		w.WriteHeader(http.StatusOK)
-		w.Write(data)
-		return
-	}
-
-	// Failure to auth
-	w.WriteHeader(http.StatusUnauthorized)
-}*/
-
-// serveWS is a HTTP Handler that the has the Manager that allows connections
 func (m *Manager) ServeWS(w http.ResponseWriter, r *http.Request) {
-	//session := getSession(r)
-	//fmt.Println("sess", session.UserID)
-	/*if session == nil {
-		// Tell the user its not authorized
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}*/
 	fmt.Println("here")
 
-	/*// Grab the OTP in the Get param
-	otp := r.URL.Query().Get("otp")
-	if otp == "" {
-		// Tell the user its not authorized
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	// Verify OTP is existing
-	if !m.otps.VerifyOTP(otp) {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-	*/
 	log.Println("New connection")
-	// Begin by upgrading the HTTP request
 	conn, err := websocketUpgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	// Create New Client
 	client := NewClient(conn, m)
-	// Add the newly created client to the manager
 	m.addClient(client)
 
 	go client.readMessages()
 	go client.writeMessages()
 }
 
-// addClient will add clients to our clientList
 func (m *Manager) addClient(client *Client) {
-	// Lock so we can manipulate
 	m.Lock()
 	defer m.Unlock()
-
-	// Add Client
 	m.clients[client] = true
-
-	// Start the read / write processes
-	go client.readMessages()
-	go client.writeMessages()
 }
 
-// removeClient will remove the client and clean up
 func (m *Manager) removeClient(client *Client) {
 	m.Lock()
 	defer m.Unlock()
-
-	// Check if Client exists, then delete it
 	if _, ok := m.clients[client]; ok {
-		// close connection
 		client.connection.Close()
-		// remove
 		delete(m.clients, client)
 	}
 }
 
-// readMessages will start the client to read messages and handle them appropriatly.
-// This is supposed to be ran as a goroutine
-func (c *Client) readMessages() {
-	fmt.Println("reading messages1")
-	defer func() {
-		// Gracefully close the connection once this function is done
-		c.manager.removeClient(c)
-	}()
-	// Set Max Size of Messages in Bytes
-	c.connection.SetReadLimit(512)
-	// Configure Wait time for Pong response, use Current time + pongWait
-	// This has to be done here to set the first initial timer.
-	if err := c.connection.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
-		log.Println(err)
-		return
-	}
-	// Configure how to handle Pong responses
-	c.connection.SetPongHandler(c.pongHandler)
-
-	// Loop Forever
-	for {
-		// ReadMessage is used to read the next message in queue
-		// in the connection
-		_, payload, err := c.connection.ReadMessage()
-
-		if err != nil {
-			// If Connection is closed, we will Recieve an error here
-			// We only want to log Strange errors, but simple Disconnection
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error reading message: %v", err)
-			}
-			break // Break the loop to close conn & Cleanup
-		}
-		// Marshal incoming data into a Event struct
-		var request Event
-		if err := json.Unmarshal(payload, &request); err != nil {
-			log.Printf("error marshalling message: %v", err)
-			break // Breaking the connection here might be harsh xD
-		}
-		// Route the Event
-		if err := c.manager.routeEvent(request, c); err != nil {
-			log.Println("Error handeling Message: ", err)
-		}
-	}
-}
-
-// pongHandler is used to handle PongMessages for the Client
-func (c *Client) pongHandler(pongMsg string) error {
-	// Current time + Pong Wait time
-	log.Println("pong")
-	return c.connection.SetReadDeadline(time.Now().Add(pongWait))
-}
-
-// writeMessages is a process that listens for new messages to output to the Client
-func (c *Client) writeMessages() {
-	// Create a ticker that triggers a ping at given interval
-	ticker := time.NewTicker(pingInterval)
-	defer func() {
-		ticker.Stop()
-		// Graceful close if this triggers a closing
-		c.manager.removeClient(c)
-	}()
-
-	for {
-		select {
-		case message, ok := <-c.egress:
-			// Ok will be false Incase the egress channel is closed
-			if !ok {
-				// Manager has closed this connection channel, so communicate that to frontend
-				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
-					// Log that the connection is closed and the reason
-					log.Println("connection closed: ", err)
-				}
-				// Return to close the goroutine
-				return
-			}
-
-			data, err := json.Marshal(message)
-			if err != nil {
-				log.Println(err)
-				return // closes the connection, should we really
-			}
-			// Write a Regular text message to the connection
-			if err := c.connection.WriteMessage(websocket.TextMessage, data); err != nil {
-				log.Println(err)
-			}
-			log.Println("sent message")
-		case <-ticker.C:
-			log.Println("ping")
-			// Send the Ping
-			if err := c.connection.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
-				log.Println("writemsg: ", err)
-				return // return to break this goroutine triggeing cleanup
-			}
-		}
-
-	}
-}
-
-func WSHandler(w http.ResponseWriter, r *http.Request) {
-	manager := NewManager()
-	manager.ServeWS(w, r)
-}
-
-// CLIENT
-
-var (
-	// pongWait is how long we will await a pong response from client
-	pongWait = 10 * time.Second
-	// pingInterval has to be less than pongWait, We cant multiply by 0.9 to get 90% of time
-	// Because that can make decimals, so instead *9 / 10 to get 90%
-	// The reason why it has to be less than PingRequency is becuase otherwise it will send a new Ping before getting response
-	pingInterval = (pongWait * 9) / 10
-)
-
-// ClientList is a map used to help manage a map of clients
 type ClientList map[*Client]bool
 
-// Client is a websocket client, basically a frontend visitor
 type Client struct {
-	// the websocket connection
 	connection *websocket.Conn
-
-	// manager is the manager used to manage the client
-	manager *Manager
-	// egress is used to avoid concurrent writes on the WebSocket
-	egress chan Event
+	manager    *Manager
+	egress     chan Event
+	writeMutex sync.Mutex
 }
 
-// NewClient is used to initialize a new Client with all required values initialized
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
@@ -343,34 +117,106 @@ func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	}
 }
 
-// EVENTS
+func (c *Client) readMessages() {
+	fmt.Println("reading messages1")
+	defer func() {
+		c.manager.removeClient(c)
+	}()
+	c.connection.SetReadLimit(512)
+	if err := c.connection.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		log.Println(err)
+		return
+	}
+	c.connection.SetPongHandler(c.pongHandler)
 
-// Event is the Messages sent over the websocket
-// Used to differ between different actions
+	for {
+		_, payload, err := c.connection.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error reading message: %v", err)
+			}
+			break
+		}
+		var request Event
+		if err := json.Unmarshal(payload, &request); err != nil {
+			log.Printf("error marshalling message: %v", err)
+			break
+		}
+		if err := c.manager.routeEvent(request, c); err != nil {
+			log.Println("Error handeling Message: ", err)
+		}
+	}
+}
+
+func (c *Client) pongHandler(pongMsg string) error {
+	log.Println("pong")
+	return c.connection.SetReadDeadline(time.Now().Add(pongWait))
+}
+
+func (c *Client) writeMessages() {
+	ticker := time.NewTicker(pingInterval)
+	defer func() {
+		ticker.Stop()
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.egress:
+			if !ok {
+				c.write(websocket.CloseMessage, nil)
+				return
+			}
+			data, err := json.Marshal(message)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			if err := c.write(websocket.TextMessage, data); err != nil {
+				log.Println(err)
+			}
+			log.Println("sent message")
+		case <-ticker.C:
+			log.Println("ping")
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				log.Println("writemsg: ", err)
+				return
+			}
+		}
+	}
+}
+
+func (c *Client) write(messageType int, data []byte) error {
+	c.writeMutex.Lock()
+	defer c.writeMutex.Unlock()
+	return c.connection.WriteMessage(messageType, data)
+}
+
+func WSHandler(w http.ResponseWriter, r *http.Request) {
+	manager := NewManager()
+	manager.ServeWS(w, r)
+}
+
+var (
+	pongWait     = 10 * time.Second
+	pingInterval = (pongWait * 9) / 10
+)
+
 type Event struct {
-	// Type is the message type sent
-	Type string `json:"type"`
-	// Payload is the data Based on the Type
+	Type    string          `json:"type"`
 	Payload json.RawMessage `json:"payload"`
 }
 
-// EventHandler is a function signature that is used to affect messages on the socket and triggered
-// depending on the type
 type EventHandler func(event Event, c *Client) error
 
 const (
-	// EventSendMessage is the event name for new chat messages sent
 	EventSendMessage = "send_message"
 )
 
-// SendMessageEvent is the payload sent in the
-// send_message event
 type SendMessageEvent struct {
 	Message string `json:"message"`
 	From    string `json:"from"`
 }
-
-// One Time Password (OTP)
 
 type OTP struct {
 	Key     string
@@ -379,55 +225,41 @@ type OTP struct {
 
 type RetentionMap map[string]OTP
 
-// NewRetentionMap will create a new retentionmap and start the retention given the set period
 func NewRetentionMap(ctx context.Context, retentionPeriod time.Duration) RetentionMap {
 	rm := make(RetentionMap)
-
 	go rm.Retention(ctx, retentionPeriod)
-
 	return rm
 }
 
-// NewOTP creates and adds a new otp to the map
 func (rm RetentionMap) NewOTP() OTP {
 	o := OTP{
 		Key:     uuid.NewString(),
 		Created: time.Now(),
 	}
-
 	rm[o.Key] = o
 	return o
 }
 
-// VerifyOTP will make sure a OTP exists
-// and return true if so
-// It will also delete the key so it cant be reused
 func (rm RetentionMap) VerifyOTP(otp string) bool {
-	// Verify OTP is existing
 	if _, ok := rm[otp]; !ok {
-		// otp does not exist
 		return false
 	}
 	delete(rm, otp)
 	return true
 }
 
-// Retention will make sure old OTPs are removed
-// Is Blocking, so run as a Goroutine
 func (rm RetentionMap) Retention(ctx context.Context, retentionPeriod time.Duration) {
 	ticker := time.NewTicker(400 * time.Millisecond)
 	for {
 		select {
 		case <-ticker.C:
 			for _, otp := range rm {
-				// Add Retention to Created and check if it is expired
 				if otp.Created.Add(retentionPeriod).Before(time.Now()) {
 					delete(rm, otp.Key)
 				}
 			}
 		case <-ctx.Done():
 			return
-
 		}
 	}
 }

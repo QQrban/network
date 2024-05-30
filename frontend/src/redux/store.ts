e949967b7c29789b0af825ac132540ce30379db8
Kurban Ramazanov <kurban.ramazanovv@gmail.com>
2024-05-30 22:11:08 +0300

    message slice

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import profileReducer from "./features/profile/profileSlice";
 import suggestionsReducer from "./features/suggestions/suggestionsSlice";
+import initmessageReducer from "./features/initMessage/initeMessageSlice";
 
 export const store = configureStore({
   reducer: {
     authReducer,
     profileReducer,
     suggestionsReducer,
+    initmessageReducer,
   },
 });
 

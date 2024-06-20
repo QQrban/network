650532185e8e9266b7fdfbb2aa1ebb955dd8ce96
toomasv <toomas.vooglaid@gmail.com>
2024-06-20 15:19:58 +0300

    put CORS back

--- backend/main.go
+++ backend/main.go
@@ -17,6 +17,7 @@ func CORS(next http.Handler) http.Handler {
 		if frontendAddress == "" {
 			frontendAddress = "http://localhost:3000"
 		}
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
